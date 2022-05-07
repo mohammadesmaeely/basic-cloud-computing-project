8884805ec3d387460e658fde2626f7187740b5d1
mohammad esmaeely <esmaeely1377@gmail.com>
2022-05-07 19:15:08 +0430

    bad word: bad word model + migrations file

--- unigle_monolithic/bad_words/models.py
+++ unigle_monolithic/bad_words/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class BadWord(models.Model):
+    word = models.CharField(unique=True, max_length=300, verbose_name=_("word"))
+
+    class Meta:
+        verbose_name = _("bad word")
+        verbose_name_plural = _("bad words")
+
+    def __str__(self):
+        return self.word
