e24dd095fac06a1d73c0c3f0e9df04f0a6d8fff6
mohammad esmaeely <esmaeely1377@gmail.com>
2022-05-07 18:04:27 +0430

    assessments: assessment model + migration file

--- unigle_monolithic/assessments/models.py
+++ unigle_monolithic/assessments/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from users.models import User
+from presents.models import Present
+
+
+class Assessment(models.Model):
+    registrant = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("registrant"))
+    present = models.ForeignKey(Present, on_delete=models.CASCADE, verbose_name=_("presenting lesson"))
+    rate = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ],
+        verbose_name=_("rate")
+    )
+    comment = models.TextField(verbose_name=_("comment"))
+
+    class Meta:
+        verbose_name = _("assessment")
+        verbose_name_plural = _("assessments")
+
+    def __str__(self):
+        return f'{self.present} - {self.comment}'
+
